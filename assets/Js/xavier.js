b15aba3546d35a7412c973e49ff21f351d822a15
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2022-12-07 14:46:53 -0500

    attempted to use college football api but will be going in a different direction. leaving this script in case things change

--- assets/Js/xavier.js
+++ assets/Js/xavier.js
@@ -1,47 +1,64 @@
-function getSingleSeasonStatsByTeamAndCurrentYear(teamName="Rutgers"){
-    var queryURL = `https://api.collegefootballdata.com/stats/season?team=${teamName}&year=2022`
+function getSingleSeasonStatsByTeamAndCurrentYear(teamName = "Rutgers") {
+var queryURL = "https://api.collegefootballdata.com/stats/season?year=2022&team=Rutgers"
+
+    console.log(queryURL)
+    // https://api.collegefootballdata.com/stats/season?year=2022&team=Rutgers
     $.ajax({
         headers: {
             "Authorization": "Bearer NjbIvt5CZURrTLM3snOfwmg9qhSrVzB3UJ6Pn2JFIGmS0Zf1ANV5nkMKth9De+4W",
             "accept": "application/json",
-            "Access-Control-Allow-Origin": "https://nickj13.github.io/"
+            // "Access-Control-Allow-Origin": "https://nickj13.github.io/"
     },
         url: queryURL,
         method: "GET"
     }).then(function(res) {
         console.log(res);
     })
+    // fetch(queryURL, {
+    //     method: "GET",
+    //     headers: {
+    //         "Authorization": "Bearer NjbIvt5CZURrTLM3snOfwmg9qhSrVzB3UJ6Pn2JFIGmS0Zf1ANV5nkMKth9De+4W",
+    //         "accept": "application/json"
+    //     },
+    //     mode: "no-cors"
+    // }).then(function(response) {
+    //     return response.json()
+    // }).then(function(data) {
+    //     console.log(data)
+    // }).catch(function(err) {
+    //     console.log(err)
+    // })
 }
 
-$('.btn').click(function(event){
+$('.btn').click(function (event) {
     event.preventDefault();
     var college1 = $('#college-1').val();
     returnData(college1);
     var college2 = $('#college-2').val();
     returnData(college2);
-    
-    
-    
+
+
+
 })
 
 getSingleSeasonStatsByTeamAndCurrentYear();
 
 // year
 // integer
-// (query)	
+// (query)
 // Year filter (required if no team specified)
 
 // team
 // string
-// (query)	
+// (query)
 
 // conference
 // string
-// (query)	
+// (query)
 
 // startWeek
 // integer
-// (query)	
+// (query)
 
 // endWeek
 // integer
