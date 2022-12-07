f68926ebdcc557f91c767fb23cdf1a6d8af5e92c
ogwalrus <tommymaddog@hotmail.com>
2022-12-07 11:57:30 -0500

    Set up api and ajax

---
+++ assets/Js/script.js
@@ -0,0 +1,27 @@
+
+// var collegeTest = $(this).attr('#college-1').val();
+
+
+// console.log(queryURL);
+
+function returnData(name){
+    var splitName = name.replaceAll(' ', '%20');
+    var queryURL = 'https://api.data.gov/ed/collegescorecard/v1/schools?school.name=' + splitName + '&api_key=kh5rCjQLNGljULfkmR1gb4fSHmO157RceOJDxMCZ';
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(res) {
+        console.log(res);
+    })
+}
+
+$('.btn').click(function(event){
+    event.preventDefault();
+    var college1 = $('#college-1').val();
+    returnData(college1);
+    var college2 = $('#college-2').val();
+    returnData(college2);
+    
+    
+    
+})
