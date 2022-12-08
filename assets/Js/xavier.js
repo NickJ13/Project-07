936f483cf7cc7578ea2b015880129ba346e9d6bd
NyqEvo <117671943+NyqEvo@users.noreply.github.com>
2022-12-08 10:51:58 -0500

    added functionality to the college football api and set data to grab for the site

--- assets/Js/xavier.js
+++ assets/Js/xavier.js
@@ -1,19 +1,19 @@
-function getSingleSeasonStatsByTeamAndCurrentYear(teamName = "Rutgers") {
-var queryURL = "https://api.collegefootballdata.com/stats/season?year=2022&team=Rutgers"
+// function getSingleSeasonStatsByTeamAndCurrentYear(teamName = "Rutgers") {
+// var queryURL = "https://api.collegefootballdata.com/stats/season?year=2022&team=Rutgers"
 
-    console.log(queryURL)
-    // https://api.collegefootballdata.com/stats/season?year=2022&team=Rutgers
-    $.ajax({
-        headers: {
-            "Authorization": "Bearer NjbIvt5CZURrTLM3snOfwmg9qhSrVzB3UJ6Pn2JFIGmS0Zf1ANV5nkMKth9De+4W",
-            "accept": "application/json",
-            // "Access-Control-Allow-Origin": "https://nickj13.github.io/"
-    },
-        url: queryURL,
-        method: "GET"
-    }).then(function(res) {
-        console.log(res);
-    })
+//     console.log(queryURL)
+//     // https://api.collegefootballdata.com/stats/season?year=2022&team=Rutgers
+//     $.ajax({
+//         headers: {
+//             "Authorization": "Bearer NjbIvt5CZURrTLM3snOfwmg9qhSrVzB3UJ6Pn2JFIGmS0Zf1ANV5nkMKth9De+4W",
+//             "accept": "application/json",
+//             // "Access-Control-Allow-Origin": "https://nickj13.github.io/"
+//     },
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(res) {
+//         console.log(res);
+//     })
     // fetch(queryURL, {
     //     method: "GET",
     //     headers: {
@@ -28,20 +28,42 @@ var queryURL = "https://api.collegefootballdata.com/stats/season?year=2022&team=
     // }).catch(function(err) {
     //     console.log(err)
     // })
+// }
+
+var college1Data = []
+var college2Data = []
+
+function returnFootballStats(name) {
+    var queryURL = 'http://site.api.espn.com/apis/site/v2/sports/football/college-football/teams/' + name
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).then(function(res) {
+        // console.log(res)
+        var data = []
+        data.push(res.team.name)
+        data.push(res.team.record.items[0].summary)
+        data.push(res.team.standingSummary)
+        data.push(res.team.links[0].href)
+        console.log(data)
+        return data
+    })
 }
 
 $('.btn').click(function (event) {
     event.preventDefault();
     var college1 = $('#college-1').val();
-    returnData(college1);
+    college1Data = returnFootballStats(college1);
+    console.log(college1Data)
     var college2 = $('#college-2').val();
-    returnData(college2);
+    college2Data = returnFootballStats(college2);
+    console.log(college2Data)
 
 
 
 })
 
-getSingleSeasonStatsByTeamAndCurrentYear();
+// getSingleSeasonStatsByTeamAndCurrentYear();
 
 // year
 // integer
